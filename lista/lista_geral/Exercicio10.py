35f440c3126bf0dd10484ce487e13e3222102cc8
Pietra Ruiz <162887641+PietraCaraccoRuiz@users.noreply.github.com>
2025-05-06 21:29:49 -0300

    Update Exercicio10.py

--- lista/lista_geral/Exercicio10.py
+++ lista/lista_geral/Exercicio10.py
@@ -6,5 +6,21 @@ for palavra in palavras:
     quant = len(palavra)
     normal= []
     invertida = []
+
+    i = 0
     for i in range(quant):
-        palavra[i]
+        print(i)
+        normal += palavra[i]
+        print(normal)
+
+    j = quant - 1
+    for i in range(quant):
+        print(j)
+        invertida += palavra[j]
+        print(invertida)
+        j -= 1
+
+    if normal == invertida:
+        print("É palíndromo")
+    else:
+        print("NÃO é palíndromo")
